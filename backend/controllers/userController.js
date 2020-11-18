60961bb810c6333979d6ebceeb8ea80f50afd5f5
disharjayantha <dishuj15@gmail.com>
2020-11-18 21:56:54 +0530

    Added admin screen for editing user credentials front and backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -89,4 +89,76 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+//@desc Get all users (Admin)
+//@route GET /api/users
+//@access Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+// @desc Delete a user (Admin)
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (user) {
+    await user.remove()
+    res.json({
+      message: 'User removed',
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc Get user by ID (Admin)
+// @route GET /api/users/:id
+// @access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+// @desc update any user with id (Admin)
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    // earlier it was user.isAdmin = req.body.isAdmin || user.isAdmin if user is already admin i.e if user.isAdmin = true then we
+    // can't make it false meaning cant make isAdmin false since false || true = true, that's why use direct value from user(admin)
+    user.isAdmin = req.body.isAdmin
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updateUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+}
